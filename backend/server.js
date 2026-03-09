4637de043ad54ebf911f67d724d5ecbb682c2493
ademhmercha <ademhmerchaaa@gmail.com>
2026-03-09 09:09:11 +0100

    fix: SPA routing vercel.json + sameSite=none for cross-domain cookies

--- backend/server.js
+++ backend/server.js
@@ -64,8 +64,8 @@ app.use(session({
     cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'lax',
+        secure: true,
+        sameSite: 'none',
     }
 }));
 
